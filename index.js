1d5563cfcaea97be158048f67d96dc6849713fe7
Cr-Israel <carlosisrael08@hotmail.com>
2023-05-13 13:03:12 -0300

    backend

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+
+// Config JSON Response
+app.use(express.json());
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+
+// Public Folder for Imgaes
+app.use(express.static('public'));
+
+// Routes
+import UserRoutes from './routes/UserRoutes.js';
+import PetRoutes from './routes/PetRoutes.js';
+app.use('/users', UserRoutes);
+app.use('/pets', PetRoutes);
+
+const dbUser = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@getapet.heypw38.mongodb.net/`
+).then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}!`);
+    });
+}).catch((err) => console.log(err));
